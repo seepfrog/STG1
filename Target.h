3bd33201387770930ed13c60acba7598b4dd4434
seepfrog <cyadd20@gmail.com>
2019-02-15 06:03:42 +0900

    cppファイルの作成

--- Target.h
+++ Target.h
@@ -2,6 +2,13 @@
 
 namespace STG {
 	class Target {
-
+	public:
+		Target() {};
+		void drawTarget();
+		void getKey(int key);
+		void setPositionTarget(double xPos, double yPos);
+	private:
+		double TargetXPosition = 0.0;
+		double TargetYPosition = 0.0;
 	};
 }
